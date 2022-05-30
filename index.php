90cf668d30a50469fc67081893f73f4237d179d0
jorgprado <88862202+jorgprado@users.noreply.github.com>
2022-05-29 23:45:35 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Cadastrar</title>
+	</head>
+	<body>
+			<h1>Cadastrar Usúario</h1>
+			<form method="POST" action="processo.php">
+				<label>Nome: </label>
+				<input type="text" name="nome" placeholder="Seu nome completo"><br><br>
+
+				<label>E-mail: </label>
+				<input type="E-mail" name="email" placeholder="Seu melhor E-mail"><br><br>
+
+				<input type="submit" value="Cadastrar">
+			</form>
+	</body>
+</html>
